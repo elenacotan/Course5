0faa8dc6cf1dd4585f2adb6a2b9a1c76c2f89037
elenacotan <61800051+elenacotan@users.noreply.github.com>
2020-03-15 21:14:17 +0200

    Exercitiul3

---
+++ Curs5_ex3.py
@@ -0,0 +1,12 @@
+import Curs5_ex1
+import Curs5_ex2
+
+if __Curs5_ex1__ == '__main__':
+    numarul = int(input("Introduceti de la tastatura un numar natural:"))
+    Curs5_ex1.numar_prim(numarul)
+    Curs5_ex1.patrat_perfect(numarul)
+
+if __Curs5_ex2__ == '__main__':
+    textul =input("Introduceti textul dorit:")
+    Curs5_ex2.litera_mare(textul)
+    Curs5_ex2.dictionar_count(textul)
